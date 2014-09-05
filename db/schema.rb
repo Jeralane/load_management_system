89da8c9af0ddfd52d1511013e7497f6f562f0f6b
Jess Alejo <tbki_jea@yahoo.com>
2014-09-05 19:48:21 +0800

    Added admin column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903101601) do
+ActiveRecord::Schema.define(version: 20140905114527) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140903101601) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
 end
