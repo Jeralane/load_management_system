c535125cad8d3c5e79a62b52ec7a7f87596c00b0
Jess Alejo <tbki_jea@yahoo.com>
2014-09-09 14:18:36 +0800

    Update method and templates for creating new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,35 @@ class UsersController < ApplicationController
     @users = User.all.order(:last_name).order(:first_name)
   end
 
+  def new
+    @user = User.new
+  end
+
   def edit
-  end
-
-  def destroy
-  end
-
-  def update
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'User created.'
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :username, :email, :password)
   end
 end
